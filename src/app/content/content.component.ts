ac2f575764773f794f77d77f375d8c2b45e04ebb
KeelanHoGent <keelan.savat.y2036@student.hogent.be>
2019-03-04 18:36:24 +0100

    startpagina gemaakt

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,26 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { HideService } from "../hide.service";
 import {HeaderNameService} from "../header-name.service";
 import {FormControl, FormGroup} from "@angular/forms";
+import {Race} from "../race";
+import {Observable} from "rxjs/index";
+import {DataService} from "../data.service";
+import {AddModalComponent} from "../add-modal/add-modal.component";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
 
   status$ = this.hideService.getStatus();
-  naam = this.headerNameService.name$;
+  naam$ = this.headerNameService.name$;
+  races$: Observable<Race[]>;
   naamForm = new FormGroup({
-    naam: new FormControl(this.naam),
+    naam: new FormControl(this.naam$.getValue()),
   });
 
   constructor( public hideService: HideService,
-               public headerNameService: HeaderNameService )
+               public headerNameService: HeaderNameService,
+               public dataService: DataService,
+               private modalService: NgbModal)
   { }
 
   changeName() {
     this.headerNameService.setName(this.naamForm.value.naam);
+  }
+
+  open() {
+    this.modalService.open(AddModalComponent, {centered: true});
+  }
+
+  ngOnInit(): void {
+    this.races$ = this.dataService.races$;
   }
 }
